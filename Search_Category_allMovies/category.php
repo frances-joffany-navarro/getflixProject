b1517a66ff37b684a496ebcb88b2d3a60a7876a1
mugurlu0 <77082076+mugurlu0@users.noreply.github.com>
2021-04-01 14:41:18 +0200

    New filenames to avoid confusion and comments added

--- Search_Category_allMovies/category.php
+++ Search_Category_allMovies/category.php
@@ -13,21 +13,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styleSearchFilter.css">
 </head>
 <body>
 <input type="text" id="myInput" placeholder="Search for movie titles..">
 
+<!--crete a list for movies-->
 <ul id="myTable">
-  <!-- <tr class="header">
-    <th style="width:25%;">Title</th>
-  </tr> -->
-
   <?php
-  //movies categorised and displayed
+  //select movies by joining tables in DB 
 $sql = "select film.film_title, film.film_id, film.description, film.year_released, category.category_name from film join film_category on film.film_id = film_category.film_id join category on film_category.category_id = category.category_id";
+// make a query to fetch requested titles as an object
     $answer = $conn->query($sql);
+    //display all movies
     while($row = $answer->fetch(PDO::FETCH_OBJ)){
+        //fetch film_id, film_title and category_name and display in a list item containing a div with the picture and description for each movie
+        // <a> tag passes the id of the movie we click on to the descriptions page to display only this item
         echo "<li>
         <div><figure>
         <a href=moviedetail.php?movieId=".$row->film_id.">
@@ -40,6 +41,6 @@ $sql = "select film.film_title, film.film_id, film.description, film.year_releas
   </li>";}?>
 </ul>
 
-    <script src="script.js"></script>
+    <script src="scriptSearchFilter.js"></script>
 </body>
 </html>
--- Search_Category_allMovies/scriptSearchFilter.js
+++ Search_Category_allMovies/scriptSearchFilter.js
@@ -1,26 +1,30 @@
+//the filter will begin proceeding once whe type on any key of the keyboard and the following script will begin performing
 document.getElementById("myInput").addEventListener("keyup", () => {
-  //variables
+  //first declare variables
   let input = document.getElementById("myInput");
   let filter = input.value.toUpperCase();
   let table = document.getElementById("myTable");
   let li = table.getElementsByTagName("li");
+
+  //optional css (can be changed)
   document.body.style.backgroundColor = "#1F1C18";
   document.body.style.color = "black";
 
-// Loop through all table rows, and hide those who don't match the search query
-// Loop through first tbody's rows
+// Loop through all list items, and hide those who don't match the search query
+
+// Loop through the list items
   for (let i = 0; i < li.length; i++) {
-    // define the row's cells
+    // define the figcaption elements inside the list tags
     let figcaption = li[i].getElementsByTagName("figcaption");
-    // hide the row
+    // hide the list items
     li[i].style.display = "none";
-    // loop through row cells
+    // loop through figcaption tags at index "j"
     for (let j = 0; j < figcaption.length; j++) {
       // if there's a match
       if (figcaption[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
-        // show the row
+        // show the whole list element 
         li[i].style.display = "";
-        // skip to the next row
+        // skip to the next list element
         continue;
       }
     }
