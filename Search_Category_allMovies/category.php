b3408b389d14774304aa8a35aeda3dc65a0414f3
mugurlu0 <77082076+mugurlu0@users.noreply.github.com>
2021-04-01 12:29:17 +0200

    filter adapted and category script edited

--- Search_Category_allMovies/category.php
+++ Search_Category_allMovies/category.php
@@ -18,26 +18,27 @@
 <body>
 <input type="text" id="myInput" placeholder="Search for movie titles..">
 
-<table id="myTable">
-  <tr class="header">
+<ul id="myTable">
+  <!-- <tr class="header">
     <th style="width:25%;">Title</th>
-    <th style="width:25%;">Description</th>
-    <th style="width:25%;">Category</th>
-    <th style="width:25%;">Year released</th>
-  </tr>
+  </tr> -->
 
   <?php
   //movies categorised and displayed
 $sql = "select film.film_title, film.description, film.year_released, category.category_name from film join film_category on film.film_id = film_category.film_id join category on film_category.category_id = category.category_id";
     $answer = $conn->query($sql);
     while($row = $answer->fetch(PDO::FETCH_OBJ)){
-        echo "<tr>
-    <td>".$row->film_title."</td>
-    <td>".$row->description."</td>
-    <td>".$row->category_name."</td>
-    <td>".$row->year_released."</td>
-  </tr>";}?>
-</table>
+        echo "<li>
+        <div><figure>
+        <a href=http://localhost/Search/moviedetail.php?>
+        <img src='sample.jpg' alt=$row->film_title>
+        </a>
+        <figcaption>".$row->film_title."</figcaption>
+        <figcaption>".$row->category_name."</figcaption>
+    </figure>
+    </div>
+  </li>";}?>
+</ul>
 
     <script src="script.js"></script>
 </body>
