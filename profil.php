f48807f13dc370b3df0ef68fe8dd28d792469060
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2021-04-03 14:23:46 +0200

    move profil.css and update the link in profil.php

--- profil.php
+++ profil.php
@@ -8,7 +8,7 @@
      <!-- google font -->
      <link rel="preconnect" href="https://fonts.gstatic.com"> 
      <link href="https://fonts.googleapis.com/css2?family=Chango&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="profil.css">
+    <link rel="stylesheet" href="css/profil.css">
     <title>Document</title>
 </head>
 <body>
