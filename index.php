f2292b2c9707e08c5b19d28931bc5d9ce6acf426
Francois <francois.leleu11@gmail.com>
2021-04-06 14:59:54 +0200

    new responsivity

--- index.php
+++ index.php
@@ -1,54 +1,68 @@
+<?php include 'DB/dbConnection.php';?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>GetFlix</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style2.css">
+
+  <title>Home</title>
+
 </head>
-    <body>
-        <!----------------------------------------------------- Header Section --------------------------------->
-            <?php include("header.php") ?>
-            <!-------------------------------------------------Body and Main section--------------------------------------> 
 
-            <!---------------------GRID organisation and Row Disposition.--------------------->
-                
-                <section class="container">  
-            
-                    <div id="newMovieCategory"> <b>New Movies: </b></div>
-                    <a href="#" id="btnViewMore">View More</a>
-            
-           
-                    <article id="wideBanner">"A wide Banner of a Movie poster</article>
-                    <div id="movie1Row3"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                    <div id="movie2Row3"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                    <div id="movie3Row3"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                    <div id="movie4Row3"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-            
-                    <div id="movie1Row4"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                    <div id="movie2Row4"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                    <div id="movie3Row4"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                    <div id="movie4Row4"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-            
-                    <div id="movie1Row5"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                    <div id="movie2Row5"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                    <div id="movie3Row5"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                    <div id="movie4Row5"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-        
-                    <div id="movie1Row6"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                    <div id="movie2Row6"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                    <div id="movie3Row6"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                    <div id="movie4Row6"><a href="#"><img src="#" alt="Movie Poster"></a></div>
-                </section>
-            
-            <!-------------------------------------------------Footer-----------------------------------------> 
-    
-        <?php include("footer.php") ?>
+<body>
+  <?php include 'navbar.php';?>
+  <main>
+   
+            <ul id="myTable">
+              <?php  
+                $category = !empty($_GET['category']) ? $_GET['category'] : NULL;
+              // $category= $_GET['category'];
+              if(isset($category)){
+                $sqlCategory = "SELECT film_title, film.film_id, category_name FROM `film` join film_category on film.film_id = film_category.film_id join category on film_category.category_id = category.category_id where category.category_name = '$category'";
+                $result = $dbConnection->query($sqlCategory);
+                    //display all movies
+                    while($row = $result->fetch(PDO::FETCH_OBJ)){
+                      echo  "<li>
+                      <div><figure>
+                      <a href=moviedetail.php?movieId=".$row->film_id.">
+                      <img src='images/academy_dinosaur.jpeg' alt=$row->film_title>
+                      </a>
+                      <figcaption>".$row->film_title."</figcaption>
+                      <figcaption>".$row->category_name."</figcaption>
+                  </figure>
+                  </div>
+                </li>";
+                    }
+              }
+              else{
+                  //movies categorised and displayed
+        $sql = "select film.film_title, film.film_id, film.description, film.year_released, category.category_name from film join film_category on film.film_id = film_category.film_id join category on film_category.category_id = category.category_id";
+        $answer = $dbConnection->query($sql);
+        while($row = $answer->fetch(PDO::FETCH_OBJ)){
+            echo "<li>
+            <div><figure>
+            <a href=moviedetail.php?movieId=".$row->film_id.">
+            <img src='images/academy_dinosaur.jpeg' alt=$row->film_title>
+            </a>
+            <figcaption>".$row->film_title."</figcaption>
+            <figcaption>".$row->category_name."</figcaption>
+        </figure>
+        </div>
+        </li>";}
+              }
 
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-    </body>
+        ?>
+            </ul>
+      
+  </main>
+
+<?php include 'footer.php';?>
+<script src="js/navbar.js"></script>
+</body>
+
 </html>
--- navbar.php
+++ navbar.php
@@ -1,4 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+<?php include "DB/DBConnection.php";?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style2.css">
 
 <header>
             <strong><p>GetFlix Logo</p></strong>
@@ -11,23 +14,26 @@
             Movies by Genre
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+             <?php
+          $sql = "SELECT category_name FROM `category`";
+          $answer = $dbConnection->query($sql);
+    //display all movies
+    while($row = $answer->fetch(PDO::FETCH_OBJ)){
+        echo    "<li><a class=dropdown-item href=finalIndex.php?category=".$row->category_name.">".$row->category_name."</a></li>";}?>
           </ul>
         </li>
-        
+        <div class="col-auto" id="accountButton">
+                          <button type="submit" class="btn btn-primary mb-3"><a href="profil.php" style="color:white;text-decoration:none;">Your Account</a></button>
+
+        </div>
                     <div class="input-group mb-3" id="searchBar">
-                        <input type="text" class="form-control" placeholder="" aria-label="Recipient's username" aria-describedby="button-addon2">
-                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
-                        
+                    <input type="text" id="myInput" value="<?php echo !empty($_GET['search']) ? $_GET['search'] : NULL;?>" class="form-control" placeholder="Title/category" aria-label="Recipient's username" aria-describedby="button-addon2">
+                        <button class="btn btn-outline-secondary" type="button" name="submit" id="button-addon2">Search</button>
                       </div>
-                        <div class="col-auto" id="accountButton">
-                          <button type="submit" class="btn btn-primary mb-3">Your Account</button>
-                        </div>
+                       
                     
             </div>
         </header>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+<script src="js\searchInput.js"></script>
+<script src="js\scriptSearchFilter.js"></script>
