d151ebad993595d2ebef7fc660ce99a260421adc
mugurlu0 <77082076+mugurlu0@users.noreply.github.com>
2021-04-07 14:58:32 +0200

    ternary operator added to fix bug

--- index.php
+++ index.php
@@ -44,10 +44,7 @@
                   session_start();
                   // Page was not reloaded via a button press
                   
-                  $index=$_SESSION['index'];
-                  if (!isset($_GET['more'])) {
-                      $_SESSION['index'] = 0; // Reset counter
-                  }
+                  $index=isset($_GET['more'])?$_SESSION['index']:0;                  
                
         $sql = "select film.film_title, film.film_id, film.description, film.year_released, category.category_name from film join film_category on film.film_id = film_category.film_id join category on film_category.category_id = category.category_id limit $index,100";
         $answer = $dbConnection->query($sql);
