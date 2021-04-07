063768c139051502beea24f932619b89d309287e
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2021-04-07 18:34:26 +0200

    Revert "Delete inscription.php"
    This reverts commit 44eb49d5c7e78a9e243a968163fe255acbba7d71.

---
+++ inscription.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="NoS1gnal" />
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/inscription.css">
+    <title>Connexion</title>
+</head>
+
+<body>
+    <div class="login-form">
+        <!-- mettre un petit alert  si on a une erreur lors de la connection  (voir la partie inscription_traitement.php) -->
+        <?php
+        // vérifier si la variable existe 
+        if (isset($_GET['reg_err'])) {
+
+            // si elle existe on va le stocker dans le  $err avec le htmlspecialchars
+            $err = htmlspecialchars($_GET['reg_err']);
+
+            switch ($err) {
+
+                case 'success':
+        ?>
+                    <div class="alert alert-success">
+                        <strong>Succes</strong> registration completed !
+                    </div>
+                <?php
+                    break;
+
+                case 'password':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Error</strong> passwords don't match
+                    </div>
+                <?php
+                    break;
+
+                case 'email':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Error</strong> invalid email
+                    </div>
+                <?php
+                    break;
+
+                case 'email_length':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Error</strong> email too long
+                    </div>
+                <?php
+                    break;
+
+                case 'firstName_length':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Error</strong> first name too long
+                    </div>
+                <?php
+
+                case 'lastName_length':
+
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Error</strong> last name too long
+                    </div>
+                <?php
+
+                case 'already':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Error</strong> the account already exists
+                    </div>
+        <?php
+            }
+        }
+        ?>
+
+        <form action="inscription_traitement.php" method="post">
+            <h2 class="text-center">Register</h2>
+            <div class="form-group">
+                <input type="text" name="firstName" class="form-control" placeholder="First Name" required="required" autocomplete="off">
+            </div>
+            <div class="form-group">
+                <input type="text" name="lastName" class="form-control" placeholder="Last Name" required="required" autocomplete="off">
+            </div>
+            <div class="form-group">
+                <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" class="form-control" placeholder="Password" required="required" autocomplete="off">
+            </div>
+            <div class="form-group">
+                <input type="password" name="password_retype" class="form-control" placeholder="Confirm password" required="required" autocomplete="off">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-block">Register</button>
+            </div>
+        </form>
+        <p class="text-center">Already have an account? <a href="indexBis.php">Sign in</a></p>
+    </div>
+</body>
+
+</html>
