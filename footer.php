a8a6658ce63964986ba1820bc2bc8fa482e693e5
Francois <francois.leleu11@gmail.com>
2021-04-07 10:05:19 +0200

    link

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer>
                 <article id="bottomLogo"><img src="images/logoGetFlix.png" alt="GetFlix Logo"></article>
-                <article id="bottomLinks"><a href="url">Terms and conditions</a> <a href="url">Help</a> 2021 GetFix all rights reserved.</article>
+                <article id="bottomLinks"><a href="https://github.com/frances-joffany-navarro/getflixProject/tree/main">Link to the GitHub Page</a> <a href="url">Help</a> 2021 GetFix all rights reserved.</article>
             </footer>
