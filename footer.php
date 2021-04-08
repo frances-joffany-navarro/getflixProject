b187921b98af7f4cfdb068bd62ffd5d7d7c12bcb
Francois <francois.leleu11@gmail.com>
2021-04-08 14:06:18 +0200

    navbar colors and logo and footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer>
-                <article id="bottomLogo"><img src="images/logoGetFlix.png" alt="GetFlix Logo"></article>
-                <article id="bottomLinks"><a href="https://github.com/frances-joffany-navarro/getflixProject/tree/main">Link to the GitHub Page</a> <a href="url">Help</a> 2021 GetFix all rights reserved.</article>
-            </footer>
+    <article id="bottomLogo"><a href="index.php"><img src="images/logoGetFlix.png" alt="GetFlix Logo"></a></article>
+    <article id="bottomLinks"><a href="https://github.com/frances-joffany-navarro/getflixProject/tree/main">Link to the GitHub Page</a> <a href="url">Help</a> 2021 GetFix all rights reserved.</article>
+</footer>
