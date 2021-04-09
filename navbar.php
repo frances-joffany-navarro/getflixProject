b830be63cc78005d9124eddf05a50e8fc19a17c7
François Leleu <77114825+FrancoisLeleu@users.noreply.github.com>
2021-04-09 09:25:49 +0200

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -6,7 +6,7 @@
 <header>
 <nav class="navbar navbar-expand-lg mb-3">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">GetFlix</a>
+    <a class="navbar-brand" href="index.php"><img src="images/logoGetFlix.png" alt="GetFlix Logo"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
