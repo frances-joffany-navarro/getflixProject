8834fa529b884018b72d391c091c53243a54aa61
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2021-04-11 14:59:23 +0200

    included navbar

--- newLanding.php
+++ newLanding.php
@@ -12,61 +12,33 @@ $user = $_SESSION['user'];
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Espace membre</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>User Profile</title>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    
     <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="newLanding.css">
+    <link rel="stylesheet" href="css/newLanding.css">
   </head>
   <body>
-        <section class="sectionOne">
-            
-
-            <p class="GETFLIX">GETFLIX</p>
-            <p class="LOGO">LOGO</p>
-
-            <button class="logout" type="submit">Logout</button>
-
-
-        </section>
-
-
-        
-        
-     
-        <div class="container">
-            <section> <img class="image" src="img/avatar-profilPhp.jpg" alt=""></section>
-            <div class="col-md-12">
-
-
-                <div class="text-center">
-                        <h1 class="p-5"> <?php echo $user->firstName; ?></h1>
-                        <hr />
-                        <h1 class="p-5"><?php echo $user->lastName; ?></h1>
-                        <hr>
-                        <h4 class="p-5"><?php echo $user->email; ?></h4>
-
-
-                        <a href="deconnexion.php" class="btn btn-danger btn-lg">Déconnexion</a>
-        
-                </div>
-            </div>
-        </div>    
-
-       
-                                
-    
-    
+  <?php include 'navbar.php';?>    
+    <div class="container text-center mt-5 pt-5">
+      <img class="img-fluid mb-3" src="images/avatar-profilPhp.jpg" alt="profile">
+      <div class="col-12">
+        <div class="text-center">
+          <h1><?php echo $user->firstName . " " . $user->lastName; ?></h1>
+          <h5><?php echo $user->email; ?></h5>      
+        </div>
+      </div>
+    </div>  
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>    
   </body>
 </html>
